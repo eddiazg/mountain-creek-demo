ad679777cb1db4fd18b5726faea451553b2a7b0c
JoyBB <JoyBB@JOYPC.local>
2025-01-10 16:35:38 -0600

    Actualizar sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,30 +1,41 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 import "../styles/sidebar.css";
 import logo from '../commons/image.png'; // Asegúrate de tener la imagen en public
 
 const Sidebar = () => {
-  return (
-<div className="sidebar d-flex flex-column">
-    <div className="logo mb-4 text-center">
-        <img src={logo} alt="Logo" className="logo-img" />
-    </div>
-    <Nav defaultActiveKey="#home" className="flex-column">
-    <Nav.Link href="/dashboard" className="menu-item">
-        <span className="icon">🗒️</span>
-        <span className="text">Dashboard</span>
-    </Nav.Link>
-    <Nav.Link href="/investment" className="menu-item active">
-        <span className="icon">📊</span>
-        <span className="text">Investment</span>
-    </Nav.Link>
-    <Nav.Link href="/" className="menu-item">
-        <span className="icon">🛫</span>
-        <span className="text">Logout</span>
-    </Nav.Link>
-    </Nav>
-</div>
-  );
+const location = useLocation(); // Get the current location
+    return (
+        <div className="sidebar d-flex flex-column">
+            <div className="logo mb-4 text-center">
+                <img src={logo} alt="Logo" className="logo-img" />
+            </div>
+            <Nav className="flex-column">
+                <Nav.Link
+                href="/dashboard"
+                className={`menu-item ${location.pathname === "/dashboard" ? "active" : ""}`}
+                >
+                <span className="icon">🗒️</span>
+                <span className="text">Dashboard</span>
+                </Nav.Link>
+                <Nav.Link
+                href="/investment"
+                className={`menu-item ${location.pathname === "/investment" ? "active" : ""}`}
+                >
+                <span className="icon">📊</span>
+                <span className="text">Investment</span>
+                </Nav.Link>
+                <Nav.Link
+                href="/"
+                className={`menu-item ${location.pathname === "/" ? "active" : ""}`}
+                >
+                <span className="icon">🛫</span>
+                <span className="text">Logout</span>
+                </Nav.Link>
+            </Nav>
+        </div>
+    );
 };
- 
+
 export default Sidebar;
